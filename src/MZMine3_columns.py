42fa5bed9a011c8da4de5ae3a7843b6121c5127b
wjnbb <william.nash@bacto.bio>
2023-03-17 11:36:54 +0000

    Extracts all the column names from the MZMine3 peak table to facilitate organisation/manipulation of the peak table.

---
+++ src/MZMine3_columns.py
@@ -0,0 +1,7 @@
+def mzmine3_cols(peak_table):
+
+    #get all column names to allow subsequent organisation of samples names and table subsections
+
+    pt_cols = list(peak_table.columns)
+    print(pt_cols[0:10])
+    return pt_cols
