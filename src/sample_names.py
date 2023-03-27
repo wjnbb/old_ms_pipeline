6d9f5442d926379f73f94cc04917f209ff904790
wjnbb <william.nash@bacto.bio>
2023-03-27 17:02:07 +0100

    Extracts sample names from column headers

---
+++ src/sample_names.py
@@ -0,0 +1,11 @@
+def get_sample_names(height_df):
+
+    # Pattern match on column names in the peak height data frame
+    # take everything after datafile: to get the sample names
+
+    sample_names_full = [name.split(":")[1] for name in height_df.columns]
+    sample_names = [name.split("-")[1] for name in sample_names_full]
+
+    print(sample_names[0:10])
+
+    return sample_names
