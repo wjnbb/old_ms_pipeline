2e988b0009121480bdb1ebe607334b3e289300c2
wjnbb <william.nash@bacto.bio>
2023-03-30 16:34:21 +0100

    added type annotations and docstrings

--- src/Blank_Filtering.py
+++ src/Blank_Filtering.py
@@ -3,12 +3,21 @@ import pandas as pd
 from assign_global_variables import Blank_thresh
 
 
-def blank_filter(Bio_stats, Media_stats, QC_stats, Blank_stats):
+def blank_filter(
+    Bio_stats: pd.DataFrame,
+    Media_stats: pd.DataFrame,
+    QC_stats: pd.DataFrame,
+    Blank_stats: pd.DataFrame,
+) -> list:
+    """
+    Identifies all features rows for which no biological, media or QC sample height in the dataset exceeded a FC of 10
+    versus the average intensity of the blank samples and returns the indices of those rows."
+    """
 
     all_stats = pd.concat([Bio_stats, Media_stats, QC_stats], axis=1)
     all_stats = all_stats[[s for s in all_stats.columns if "avg" in s]]
 
-    Blank_stats['Blank_avg'] = Blank_stats['Blank_avg'].fillna(10)
+    Blank_stats["Blank_avg"] = Blank_stats["Blank_avg"].fillna(10)
 
     B_FC_table = pd.DataFrame()
 
