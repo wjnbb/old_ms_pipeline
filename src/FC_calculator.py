59af0cae95ad0918cb666a6420ace69eb8ef71b1
wjnbb <william.nash@bacto.bio>
2023-06-05 14:33:19 +0100

    replaces nans in the peak height avg data used for the FC calculation with a value of 10

--- src/FC_calculator.py
+++ src/FC_calculator.py
@@ -18,38 +18,63 @@ def FC_MultiBio_vs_MultiControl(
     bio_media_pairs = [b.split(Bio_identifier)[1] for b in Bio_groups]
     FC_table = pd.DataFrame()
 
-    # calculate FC
-    for b in bio_media_pairs:
+    #Fill the nans with 10s to ensure no nans in the FC output table
+    Bio_stats = Bio_stats[[s for s in Bio_stats.columns if "avg" in s]].fillna(10)
+    Media_stats = Media_stats[[s for s in Media_stats.columns if "avg" in s]].fillna(10)
 
-        bios = [s for s in Bio_stats.columns if b in s]
-        bios = [s for s in bios if "avg" in s]
-        bios = Bio_stats[bios].squeeze()
+    if(bio_media_linkers == "NA"):
 
-        medias = [i for i in Media_stats.columns if b in i]
+        # calculate FC
+        for b in bio_media_pairs:
+            bios = [s for s in Bio_stats.columns if b in s]
+            bios = [s for s in bios if "avg" in s]
+            bios = Bio_stats[bios].squeeze()
 
-        if len(medias) == 0:
-
-            for L in bio_media_linkers:
-
-                if L in b:
-
-                    medias = [i for i in Media_stats.columns if L in i]
-                    medias = [i for i in medias if "avg" in i]
-                    medias = Media_stats[medias].squeeze()
-
-                    FC = bios / medias
-
-                    FC_table.insert(0, ("FC" + b), FC)
-
-        else:
-
+            medias = [i for i in Media_stats.columns if b in i]
             medias = [i for i in medias if "avg" in i]
             medias = Media_stats[medias].squeeze()
 
+
             FC = bios / medias
+
             FC_table.insert(0, ("FC" + b), FC)
 
-    return FC_table
+        return FC_table
+
+    else:
+
+        # calculate FC
+        for b in bio_media_pairs:
+
+            bios = [s for s in Bio_stats.columns if b in s]
+            bios = [s for s in bios if "avg" in s]
+            bios = Bio_stats[bios].squeeze()
+
+            medias = [i for i in Media_stats.columns if b in i]
+
+            if len(medias) == 0:
+
+                for L in bio_media_linkers:
+
+                    if L in b:
+
+                        medias = [i for i in Media_stats.columns if L in i]
+                        medias = [i for i in medias if "avg" in i]
+                        medias = Media_stats[medias].squeeze()
+
+                        FC = bios / medias
+
+                        FC_table.insert(0, ("FC" + b), FC)
+
+            else:
+
+                medias = [i for i in medias if "avg" in i]
+                medias = Media_stats[medias].squeeze()
+
+                FC = bios / medias
+                FC_table.insert(0, ("FC" + b), FC)
+
+        return FC_table
 
 
 def FC_MultiBio_vs_SingleControl(
@@ -64,6 +89,10 @@ def FC_MultiBio_vs_SingleControl(
     # CALCULATE FC FOR EACH BIOLOGICAL WELL VS ITS MEDIA CONTROL
     # get character strings to allow pairing of relevant biological and media samples
     FC_table = pd.DataFrame()
+
+    # Fill the nans with 10s to ensure no nans in the FC output table
+    Bio_stats = Bio_stats[[s for s in Bio_stats.columns if "avg" in s]].fillna(10)
+    Control_stats = Control_stats[[s for s in Control_stats.columns if "avg" in s]].fillna(10)
 
     # calculate FC
     for b in Bio_groups:
@@ -93,6 +122,10 @@ def FC_SingleBio_vs_SingleControl(
     ################################################################################
     # CALCULATE FC for a single biological group vs a single media/control group
     FC_table = pd.DataFrame()
+
+    # Fill the nans with 10s to ensure no nans in the FC output table
+    Bio_stats = Bio_stats[[s for s in Bio_stats.columns if "avg" in s]].fillna(10)
+    Media_stats = Media_stats[[s for s in Media_stats.columns if "avg" in s]].fillna(10)
 
     bios = Bio_stats.iloc[:, 2]
     medias = Media_stats.iloc[:, 2]
