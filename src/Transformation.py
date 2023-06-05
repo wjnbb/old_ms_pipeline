3fda4fb8df17299ff156e8bac7f39968f851ef48
wjnbb <william.nash@bacto.bio>
2023-06-05 14:56:15 +0100

    added log, log10 and log2 transformation functions for single dataframes and mulitple dataframes.

---
+++ src/Transformation.py
@@ -0,0 +1,53 @@
+import pandas as pd
+import numpy as np
+
+def transformation_multi(norm_heights: tuple[pd.DataFrame], trans_type: str):
+
+    """Transforms all average peak height columns in tuple of dataframes supplied. Use either "log", "log10" or "log2"
+     to specify the transformation type. Returns a list of transformed dataframes."""
+
+    norm_heights = list(norm_heights)
+    trans_heights = list()
+
+    for df in norm_heights:
+
+        if trans_type == "log":
+
+            df = df.apply(np.log)
+            trans_heights.append(df)
+
+        elif trans_type == "log10":
+
+            df = df.apply(np.log10)
+            trans_heights.append(df)
+
+        elif trans_type == "log2":
+
+            df = df.apply(np.log2)
+            trans_heights.append(df)
+
+    return trans_heights
+
+
+def transformation_single(df: pd.DataFrame, trans_type: str):
+
+    """Transforms all average peak height columns in the supplied dataframe. Use either "log", "log10" or "log2"
+     to specify the transformation type. Returns a transformed dataframe."""
+
+    df = df[[s for s in df.columns if "avg" in s]]
+
+    if trans_type == "log":
+
+        df = df.apply(np.log)
+
+    elif trans_type == "log10":
+
+        df = df.apply(np.log10)
+
+    elif trans_type == "log2":
+
+        df = df.apply(np.log2)
+
+    return df
+
+
