8d34dc4ce65ece60aa9dece97fbb73e069a56e95
William Nash <william.nash@bacto.bio>
2023-07-14 16:08:00 +0100

    added global variables file

---
+++ src/assign_global_variables.py
@@ -0,0 +1,14 @@
+#Workflow1
+path = "C:/Users/WilliamNash/Bactobio Dropbox/Baccuico/LAB Work/Lab Work - Will/Mass_spec_data/20230630_JB_BI12_MeOH/MZMine3/"
+batch_name = "20230630_JB_BI12_MeOH"
+MS_system = "Sciex"
+peaklist = "peaklist.csv"
+Bio_identifier = "BI12"
+Blank_identifier = "Blank"
+Media_identifier = "Media"
+QC_identifier = "QC"
+bio_media_linkers = "NA"
+Blank_thresh = 10
+FC_threshold = 10
+RSD_threshold = 30
+peak_height_threshold = 10000
